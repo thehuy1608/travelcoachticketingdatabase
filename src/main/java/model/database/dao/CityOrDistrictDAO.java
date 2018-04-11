0d4f08c06591ed45f51e338804089c7175e9745c
huynguyen <huynt1608@gmail.com>
2018-04-11 14:47:29 +0700

    Add StationDAO and CityOrDistrictDAO

--- src/main/java/model/database/dao/CityOrDistrictDAO.java
+++ src/main/java/model/database/dao/CityOrDistrictDAO.java
@@ -7,6 +7,7 @@ package model.database.dao;
 
 import java.util.List;
 import model.database.hibernate.HibernateUtil;
+import model.database.pojo.Cityordistrict;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -16,19 +17,100 @@ import org.hibernate.Session;
  * @author User
  */
 public class CityOrDistrictDAO {
-    public static void main(String[] args) {
+
+    /**
+     *Get all Cityordistrict object in database
+     * @return
+     */
+    public static List<Cityordistrict> get_city_or_district_list() {
+        List<Cityordistrict> city_or_district_list = null;
+        Session hibernate_session = HibernateUtil.getSessionFactory().openSession();
+        hibernate_session.beginTransaction();
+        try {
+            String hql = "SELECT city_or_district FROM Cityordistrict city_or_district";
+            Query query = hibernate_session.createQuery(hql);
+            city_or_district_list = query.list();
+        } catch (HibernateException e) {
+            hibernate_session.flush();
+            hibernate_session.close();
+        }
+        hibernate_session.flush();
+        hibernate_session.close();
+        return city_or_district_list;
+
+    }
+
+    /**
+     * Get the list of names of all cities and districts in database
+     *
+     * @return
+     */
+    public static List<String> get_city_or_district_name_list() {
+        List<String> city_or_district_name_list = null;
         Session hibernate_session = HibernateUtil.getSessionFactory().openSession();
         hibernate_session.beginTransaction();
         try {
             String hql = "SELECT cityOrDistrictName FROM Cityordistrict";
             Query query = hibernate_session.createQuery(hql);
-            List<String> list = query.list();
-            list.forEach((name) -> {
-                System.out.println(name);
-            });
+            city_or_district_name_list = query.list();
+        } catch (HibernateException e) {
             hibernate_session.flush();
             hibernate_session.close();
-        } catch (HibernateException e) {
         }
+        hibernate_session.flush();
+        hibernate_session.close();
+        return city_or_district_name_list;
+    }
+
+    /**
+     * Get the Cityordistrict object by its ID.
+     *
+     * @param city_or_district_id the id of the Cityordisctrict, from 1 to 64
+     * @return Cityordistrict object, null if the id doesn't exist.
+     */
+    public static Cityordistrict get_city_or_district_by_id(int city_or_district_id) {
+        Cityordistrict city_or_district = null;
+        Session hibernate_session = HibernateUtil.getSessionFactory().openSession();
+        hibernate_session.beginTransaction();
+        try {
+            city_or_district = (Cityordistrict) hibernate_session.get(Cityordistrict.class, city_or_district_id);
+        } catch (HibernateException e) {
+            hibernate_session.flush();
+            hibernate_session.close();
+        }
+        hibernate_session.flush();
+        hibernate_session.close();
+        return city_or_district;
+    }
+
+    /**
+     * Get Cityordistrict object by its name, the name must be a valid city or
+     * district in Vietnam. Notice "Thành phố Hồ Chí Minh" has different name
+     * format than others.
+     *
+     * @param city_or_district_name The name of Cityordistrict in Vietnam, like
+     * "An Giang", "Tiền Giang", "Hà Nội", "Đà Nẵng", "Thành phố Hồ Chí Minh"
+     * @return Cityordistrict object, returns null if the name is incorrect, or
+     * in wrong naming format.
+     */
+    public static Cityordistrict get_city_or_district_by_name(String city_or_district_name) {
+        Cityordistrict city_or_district = null;
+        Session hibernate_session = HibernateUtil.getSessionFactory().openSession();
+        hibernate_session.beginTransaction();
+        try {
+            String hql = "SELECT city_or_district FROM Cityordistrict city_or_district WHERE city_or_district.cityOrDistrictName=:city_or_district_name";
+            Query query = hibernate_session.createQuery(hql);
+            query.setParameter("city_or_district_name", city_or_district_name);
+            List<Cityordistrict> city_or_district_list = query.list();
+            if (!city_or_district_list.isEmpty()) {
+                city_or_district = city_or_district_list.get(0);
+            }
+        } catch (HibernateException e) {
+            hibernate_session.flush();
+            hibernate_session.close();
+        }
+        hibernate_session.flush();
+        hibernate_session.close();
+        return city_or_district;
     }
 }
---
+++ src/main/java/model/database/dao/StationDAO.java
@@ -0,0 +1,190 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.database.dao;
+
+import java.util.List;
+import model.database.hibernate.HibernateUtil;
+import model.database.pojo.Cityordistrict;
+import model.database.pojo.Station;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author User
+ */
+public class StationDAO {
+
+    /**
+     * Get the list of all Station objects in database
+     *
+     * @return
+     */
+    public static List<Station> get_station_list() {
+        List<Station> station_list = null;
+        Session hibernate_session = HibernateUtil.getSessionFactory().openSession();
+        hibernate_session.beginTransaction();
+        try {
+            String hql = "SELECT station FROM Station station";
+            Query query = hibernate_session.createQuery(hql);
+            station_list = query.list();
+        } catch (HibernateException e) {
+            hibernate_session.flush();
+            hibernate_session.close();
+        }
+        hibernate_session.flush();
+        hibernate_session.close();
+        return station_list;
+    }
+
+    /**
+     * Get the list of name of all Station objects in database
+     *
+     * @return
+     */
+    public static List<String> get_station_name_list() {
+        List<String> station_name_list = null;
+        Session hibernate_session = HibernateUtil.getSessionFactory().openSession();
+        hibernate_session.beginTransaction();
+        try {
+            String hql = "SELECT stationName FROM Station";
+            Query query = hibernate_session.createQuery(hql);
+            station_name_list = query.list();
+        } catch (HibernateException e) {
+            hibernate_session.flush();
+            hibernate_session.close();
+        }
+        hibernate_session.flush();
+        hibernate_session.close();
+        return station_name_list;
+    }
+
+    /**
+     * Get the list of address of all Station objects in database
+     *
+     * @return
+     */
+    public static List<String> get_station_address_list() {
+        List<String> station_address_list = null;
+        Session hibernate_session = HibernateUtil.getSessionFactory().openSession();
+        hibernate_session.beginTransaction();
+        try {
+            String hql = "SELECT stationAddress FROM Station";
+            Query query = hibernate_session.createQuery(hql);
+            station_address_list = query.list();
+        } catch (HibernateException e) {
+            hibernate_session.flush();
+            hibernate_session.close();
+        }
+        hibernate_session.flush();
+        hibernate_session.close();
+        return station_address_list;
+    }
+
+    /**
+     * Get Station object by its id
+     *
+     * @param station_id
+     * @return Station object, null if the id doesn't exist
+     */
+    public static Station get_station_by_station_id(int station_id) {
+        Station station = null;
+        Session hibernate_session = HibernateUtil.getSessionFactory().openSession();
+        hibernate_session.beginTransaction();
+        try {
+            station = (Station) hibernate_session.get(Station.class, station_id);
+        } catch (HibernateException e) {
+            hibernate_session.flush();
+            hibernate_session.close();
+        }
+        hibernate_session.flush();
+        hibernate_session.close();
+        return station;
+    }
+
+    /**
+     * Get Station object by its name
+     *
+     * @param station_name
+     * @return Station object, null if the name doesn't exist or wrong naming
+     * format
+     */
+    public static Station get_station_by_station_name(String station_name) {
+        Station station = null;
+        Session hibernate_session = HibernateUtil.getSessionFactory().openSession();
+        hibernate_session.beginTransaction();
+        try {
+            String hql = "SELECT station FROM Station station WHERE station.stationName = :station_name";
+            Query query = hibernate_session.createQuery(hql);
+            query.setParameter("station_name", station_name);
+            List<Station> result = query.list();
+            if (!result.isEmpty()) {
+                station = result.get(0);
+            }
+        } catch (HibernateException e) {
+            hibernate_session.flush();
+            hibernate_session.close();
+        }
+        hibernate_session.flush();
+        hibernate_session.close();
+        return station;
+    }
+
+    /**
+     *Get the list of all station in a city or district by the cityordistrict_id
+     * @param city_or_district_id
+     * @return 
+     */
+    public static List<Station> get_station_list_by_cityordistrict_id(int city_or_district_id) {
+        List<Station> station_list = null;
+        Session hibernate_session = HibernateUtil.getSessionFactory().openSession();
+        hibernate_session.beginTransaction();
+        try {
+            String hql = "SELECT station FROM Station station WHERE station.cityordistrict.cityOrDistrictId=:city_or_district_id";
+            Query query = hibernate_session.createQuery(hql);
+            query.setParameter("city_or_district_id", city_or_district_id);
+            station_list = query.list();
+        } catch (HibernateException e) {
+            hibernate_session.flush();
+            hibernate_session.close();
+        }
+        hibernate_session.flush();
+        hibernate_session.close();
+        return station_list;
+    }
+    
+    /**
+     *Get the list of all station in a city or district by the cityordistrict OBJECT
+     * @param city_or_district
+     * @return
+     */
+    public static List<Station> get_station_list_by_cityordistrict(Cityordistrict city_or_district) {
+        List<Station> station_list = null;
+        Session hibernate_session = HibernateUtil.getSessionFactory().openSession();
+        hibernate_session.beginTransaction();
+        try {
+            String hql = "SELECT station FROM Station station WHERE station.cityordistrict=:city_or_district";
+            Query query = hibernate_session.createQuery(hql);
+            query.setParameter("city_or_district", city_or_district);
+            station_list = query.list();
+        } catch (HibernateException e) {
+            hibernate_session.flush();
+            hibernate_session.close();
+        }
+        hibernate_session.flush();
+        hibernate_session.close();
+        return station_list;
+    }
+
+    public static void main(String[] args) {
+        Cityordistrict district = CityOrDistrictDAO.get_city_or_district_by_id(33);
+        List<Station> station_list = get_station_list_by_cityordistrict(district);
+        station_list.forEach((station) -> {
+            System.out.println(station.getStationName());
+        });
+    }
+}
